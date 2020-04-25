1c1e42e11f627f6af310d8bd8e14a738fb954ded
simabui <sima_sima@live.ru>
2020-04-25 20:27:06 +0300

    Updates

---
+++ precache-manifest.776924be72b67a293cba71f7fe21f226.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d02b9bbf58bf1c5429ca",
+    "url": "/pictures/static/css/imageGallery.42e61743.chunk.css"
+  },
+  {
+    "revision": "d02b9bbf58bf1c5429ca",
+    "url": "/pictures/static/js/imageGallery.97fb71e3.chunk.js"
+  },
+  {
+    "revision": "9eaa8b75890a9028644d",
+    "url": "/pictures/static/css/main.8f09fa4d.chunk.css"
+  },
+  {
+    "revision": "9eaa8b75890a9028644d",
+    "url": "/pictures/static/js/main.f6227dd8.chunk.js"
+  },
+  {
+    "revision": "e2d24ef7118e0d0cfe42",
+    "url": "/pictures/static/js/runtime~main.200dc307.js"
+  },
+  {
+    "revision": "5d89af43d06c80656606",
+    "url": "/pictures/static/js/3.5e4e1fbe.chunk.js"
+  },
+  {
+    "revision": "8f5e3f2df5730474a3c5e4a36c58bbe1",
+    "url": "/pictures/index.html"
+  }
+];
